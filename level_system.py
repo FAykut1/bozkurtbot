41b37a87db3f61e3a553dd9b9836edadbeb87ce0
pcenng <32155221+pcenng@users.noreply.github.com>
2018-04-19 13:26:08 +0300

    Create level_system.py

---
+++ level_system.py
@@ -0,0 +1,40 @@
+import json,os.path
+
+def user_add_xp(server_id,user_id: int,xp: int):
+    if os.path.isfile("users.json"):
+        try:
+            with open('users.json','r') as fp:
+                users = json.load(fp)
+            users[server_id][user_id]['xp']+=xp
+            with open('users.json','w') as fp:
+                json.dump(users,fp,sort_keys=True,indent=4)
+        except KeyError:
+                try:
+                    with open('users.json','r') as fp:
+                        users = json.load(fp)
+                    users[server_id][user_id] = {}
+                    users[server_id][user_id]['xp'] = +xp
+                    with open('users.json', 'w') as fp:
+                        json.dump(users, fp, sort_keys=True, indent=4)
+                except KeyError:
+                    with open('users.json','r') as fp:
+                        users = json.load(fp)
+                    users[server_id] = {user_id:{}}
+                    users[server_id][user_id]['xp'] = xp
+
+                    with open('users.json', 'w') as fp:
+                        json.dump(users, fp, sort_keys=True, indent=4)
+
+    else:
+        users = {server_id:{user_id:{}}}
+        users[server_id][user_id]['xp'] = xp
+        with open('users.json','w') as fp:
+            json.dump(users,fp,sort_keys=True,indent=4)
+
+def get_xp(server_id,user_id: int):
+    if os.path.isfile('users.json'):
+        with open('users.json','r') as fp:
+            users = json.load(fp)
+        return users[server_id][user_id]['xp']
+    else:
+        return 0
