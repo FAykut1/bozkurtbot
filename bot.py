4b87cb64404155dc6766720b88a542a6d20356f5
pcenng <32155221+pcenng@users.noreply.github.com>
2018-04-23 15:24:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
 
+
+client = discord.Client()
+bot_prefix = "$."
+bot = commands.Bot(command_prefix=bot_prefix)
+
+@bot.event
+async def on_ready():
+    print("Bot Online!")
+    print("Name {}".format(bot.user.name))
+    print("ID {}".format(bot.user.id))
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say("Pong")
+
+
+bot.run("NDM1NTQ5MDc0MTEyOTA1MjM5.Db9hJg.hFq_XXOVl3soad_YT28k0h5vMHI")
