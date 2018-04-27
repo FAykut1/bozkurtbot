7f4f96827279f6db8057b15c7a8012d6e4c19c7f
pcenng <32155221+pcenng@users.noreply.github.com>
2018-04-28 02:39:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -234,12 +234,12 @@ async def on_ready():
     print("I am running on " + bot.user.name)
     print(bot.user.id)
     a = 0
-    while not bot.is_closed:
-        for server in bot.servers:
-            for member in server.members:
-                if str(member.status) == 'online':
-                    online += 1
-        await bot.close()
+
+    for server in bot.servers:
+        for member in server.members:
+            if str(member.status) == 'online':
+                online += 1
+
         
     await bot.change_presence(game=Game(name="{} online user".format(online)))
 
